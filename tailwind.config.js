873c5911d18d2d6eff1bdce68911b79216c0e9fa
williambl <william@williambl.com>
2020-02-04 20:43:57 +0000

    Add some js dependencies for tailwind to work as intended

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                "kotlin": "#f18e33",
+                "java": "#b07219",
+                "C#": "#178600",
+                "python": "#3572a5",
+
+                "forge": "#1e2d42",
+                "fabric": "#dbd0b4",
+                "unity": "#030302",
+                "discord": "#7289da"
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
