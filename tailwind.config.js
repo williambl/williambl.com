88f72a63ad45beb65b42e334cb606ab43c278597
williambl <william@williambl.com>
2020-02-05 18:43:56 +0000

    Change tags to have light text

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
                 "python": "#3572a5",
 
                 "forge": "#1e2d42",
-                "fabric": "#dbd0b4",
+                "fabric": "#9a927e",
                 "unity": "#030302",
                 "discord": "#7289da"
             }
